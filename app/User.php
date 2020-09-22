0c536c9d03dbf218bfb1fc02add026272942a59c
warren <warren856>
2020-09-22 23:34:54 +0100

    Authentification par token ok

--- app/User.php
+++ app/User.php
@@ -6,10 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laratrust\Traits\LaratrustUserTrait;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
     use LaratrustUserTrait;
+    use HasApiTokens;
     use Notifiable;
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', 'AuthController@login');
+    Route::post('/signup', 'AuthController@signup');
+
+});
 
     Route::group(['prefix' => 'users'], function () {
         Route::get('/', 'UsersControllers@index');
